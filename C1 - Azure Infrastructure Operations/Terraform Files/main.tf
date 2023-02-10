7d08f49bf9188c889a1828640ef2167394406d22
thewhitelink <dwilson05lh@gmail.com>
2023-02-10 17:47:25 -0500

    added dev tags

--- C1 - Azure Infrastructure Operations/Terraform Files/main.tf
+++ C1 - Azure Infrastructure Operations/Terraform Files/main.tf
@@ -4,18 +4,21 @@ provider "azurerm" {
 resource "azurerm_resource_group" "main" {
   name = "${var.prefix}-resources"
   location = var.location
+  tag= "Dev"
 }
 resource "azurerm_virtual_network" "main" {
   name = "${var.prefix}-vnet1"
   address_space = ["10.0.0.0/22"]
   location = azure_rm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  tag= "Dev"
 }
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
+  tag= "Dev"
 }
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
@@ -26,6 +29,7 @@ resource "azurerm_network_interface" "main" {
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
+  tag= "Dev"
 }
 resource "azurerm_linux_virtual_machine" "project1" {
   name = "${var.prefix}-webserver"
