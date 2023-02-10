215576fd4f5cab0cae8fb294f9461ca3d3aa1e5c
thewhitelink <dwilson05lh@gmail.com>
2023-02-10 17:21:50 -0500

    adjusting naming convention

--- C1 - Azure Infrastructure Operations/Terraform Files/variables.tf
+++ C1 - Azure Infrastructure Operations/Terraform Files/variables.tf
@@ -1,18 +1,15 @@
-variable "prefix" [
+variable "prefix" {
   description = "The prefix which should be used for all resources of this type"
   default = "project-1"
-]
-
+}
 variable "location" {
   description = "The Azure Region in which all resources should be created"
   default = "East US"
 }
-
 variable "username" {
   description = "default username for VMs"
   default = "thewhitelink"
 }
-
 variable "password" {
   description = "default password for VMs"
   default = "P@ssw0rd!"
