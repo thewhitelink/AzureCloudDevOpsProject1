6b75d47653d1d772c039dd31766f8bbec9e1fdd5
thewhitelink <dwilson05lh@gmail.com>
2023-02-10 17:34:40 -0500

    forgot "" for line 5/9

--- C1 - Azure Infrastructure Operations/Terraform Files/main.tf
+++ C1 - Azure Infrastructure Operations/Terraform Files/main.tf
@@ -2,11 +2,11 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "main" {
-  name = ${var.prefix}-resources
+  name = "${var.prefix}-resources"
   location = var.location
 }
 resource "azurerm_virtual_network" "main" {
-  name = ${var.prefix}-vnet1
+  name = "${var.prefix}-vnet1"
   address_space = ["10.0.0.0/22"]
   location = azure_rm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
